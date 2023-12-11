8e014ee659e70f27d3e6ab9af620d951531ef0a8
Nico <ubncr@student.kit.edu>
2023-12-11 15:54:41 +0100

    Bug fixes

--- exercises/snake_game/README.md
+++ exercises/snake_game/README.md
@@ -3,3 +3,9 @@
 2. **Objective:** The primary objective is to eat items (usually depicted as fruits or dots) that appear at random positions. Each time the snake eats an item, it grows longer.
 3. **Challenge:** The game becomes more challenging as the snake grows because the player must avoid the snake running into the screen boundaries or into itself.
 4. **Game Over:** The game ends when the snake collides with the screen boundary or itself.
+
+
+
+Importing from Packages: Explain how the from snake_game.snake_game import SnakeGame line in play_snake.py imports the SnakeGame class from our snake_game.py file within the snake_game package.
+__name__ == "__main__": Clarify that this condition is used to run the code in play_snake.py only when it is executed as the main program, not when it is imported as a module in another file.
+File and Folder Names: Make sure the names of files and folders are exactly as specified, as Python is case-sensitive and relies on correct naming for imports.
--- exercises/snake_game/src/game_logic.py
+++ exercises/snake_game/src/game_logic.py
@@ -29,10 +29,68 @@ class SnakeGame:
         self.window.bind("<Down>", lambda event: self.change_direction('Down'))
         self.window.focus_set()
 
+    def draw_snake(self):
+         for segment in self.snake_position:
+            self.canvas.create_rectangle(segment[0], segment[1], segment[0]+20, segment[1]+20, fill="green")
+
+    def draw_food(self):
+        self.canvas.create_oval(self.snake_food[0], self.snake_food[1], self.snake_food[0]+20, self.snake_food[1]+20, fill="red")
     
+    def game_loop(self):
+        if not self.game_over:
+            self.game_over = not self.move_snake()
+
+        if not self.game_over:
+            self.canvas.delete("all")  # Clear canvas
+            self.draw_snake()
+            self.draw_food()
+            self.window.after(self.snake_speed, self.game_loop)  # Schedule next update
+        else:
+            print("Game Over")
+
+    # Write your code after this line
 
 
 
+    # Solution to excercise "Snake"
+    def move_snake(self):
+        global snake_food  # Declare snake_food as global at the beginning
+
+        if snake_direction is None:
+            return True  # Skip moving the snake if no direction is set
+
+        # Calculate new head position
+        head_x, head_y = self.snake_position[0]
+        if snake_direction == 'Left':
+            head_x -= 20
+        elif snake_direction == 'Right':
+            head_x += 20
+        elif snake_direction == 'Up':
+            head_y -= 20
+        elif snake_direction == 'Down':
+            head_y += 20
+
+        new_head = [head_x, head_y]
+
+        # Check for game over conditions
+        if new_head in self.snake_position or head_x < 0 or head_x >= 600 or head_y < 0 or head_y >= 400:
+            return False  # Game over
+
+        # Move snake
+        self.snake_position.insert(0, new_head)
+
+        # Check for food collision
+        if new_head == snake_food:
+            snake_food = [random.randint(0, 29)*20, random.randint(0, 19)*20]  # New food position
+        else:
+            self.snake_position.pop()  # Remove tail segment
+
+        return True  # Continue game
+
+    def change_direction(self, new_direction):
+        global snake_direction
+        if snake_direction is None or new_direction in ['Left', 'Right', 'Up', 'Down']:
+            snake_direction = new_direction
 
     def start_game(self):
         self.game_loop()
